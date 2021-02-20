1235ec9445645ac6d135cf7d5e80c7a426a87d3d
TanmayDaga <tanmay06daga@gmail.com>
2021-02-20 15:59:08 +0530

    Except = all wworking

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,12 +14,79 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::ButtonClicked(int num)
-{
-    ui->lineEdit->setText(QString(ui->lineEdit->displayText()+QString(num)));
-}
+
 
 void MainWindow::on_pushButton_3_clicked()
 {
-    ButtonClicked(ui->pushButton_3->text().toInt());
+    ui->lineEdit->setText(ui->lineEdit->displayText()+"9");
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    ui->lineEdit->setText(ui->lineEdit->displayText()+"8");
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    ui->lineEdit->setText(ui->lineEdit->displayText()+"7");
+}
+
+void MainWindow::on_pushButton_8_clicked()
+{
+    ui->lineEdit->setText(ui->lineEdit->displayText()+"6");
+}
+
+void MainWindow::on_pushButton_7_clicked()
+{
+    ui->lineEdit->setText(ui->lineEdit->displayText()+"5");
+}
+
+void MainWindow::on_pushButton_6_clicked()
+{
+    ui->lineEdit->setText(ui->lineEdit->displayText()+"4");
+}
+
+void MainWindow::on_pushButton_12_clicked()
+{
+    ui->lineEdit->setText(ui->lineEdit->displayText()+"3");
+}
+
+void MainWindow::on_pushButton_11_clicked()
+{
+    ui->lineEdit->setText(ui->lineEdit->displayText()+"2");
+}
+
+void MainWindow::on_pushButton_10_clicked()
+{
+    ui->lineEdit->setText(ui->lineEdit->displayText()+"1");
+}
+
+void MainWindow::on_pushButton_16_clicked()
+{
+    ui->lineEdit->setText(ui->lineEdit->displayText()+"0");
+}
+
+void MainWindow::on_pushButton_15_clicked()
+{
+    ui->lineEdit->setText(ui->lineEdit->displayText()+".");
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->lineEdit->setText(ui->lineEdit->displayText()+"+");
+}
+
+void MainWindow::on_pushButton_5_clicked()
+{
+    ui->lineEdit->setText(ui->lineEdit->displayText()+"-");
+}
+
+void MainWindow::on_pushButton_9_clicked()
+{
+    ui->lineEdit->setText(ui->lineEdit->displayText()+"*");
+}
+
+void MainWindow::on_pushButton_13_clicked()
+{
+    ui->lineEdit->setText(ui->lineEdit->displayText()+"/");
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include<QString>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,11 +16,40 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+
 private slots:
     void on_pushButton_3_clicked();
 
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_8_clicked();
+
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_12_clicked();
+
+    void on_pushButton_11_clicked();
+
+    void on_pushButton_10_clicked();
+
+    void on_pushButton_16_clicked();
+
+    void on_pushButton_15_clicked();
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_9_clicked();
+
+    void on_pushButton_13_clicked();
+
 private:
     Ui::MainWindow *ui;
-    void ButtonClicked(int);
+
 };
 #endif // MAINWINDOW_H
